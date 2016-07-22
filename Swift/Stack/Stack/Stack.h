96e9ee9c6f67024916d57de698f8d803d9ef09c1
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-22 15:34:56 +0200

    New Stack project with emtpy stack class implementation

---
+++ Swift/Stack/Stack/Stack.h
@@ -0,0 +1,19 @@
+//
+//  Stack.h
+//  Stack
+//
+//  Created by Stefan Mehnert on 22/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stack.
+FOUNDATION_EXPORT double StackVersionNumber;
+
+//! Project version string for Stack.
+FOUNDATION_EXPORT const unsigned char StackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stack/PublicHeader.h>
+
+
---
+++ Swift/Stack/Stack/Stack.swift
@@ -0,0 +1,21 @@
+//
+//  Stack.swift
+//  Stack
+//
+//  Created by Stefan Mehnert on 22/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+
+class Stack<T>
+{
+    func push(element: T) {
+        
+    }
+    
+    func pop() -> T? {
+        
+        return nil
+    }
+}
