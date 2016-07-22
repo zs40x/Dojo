738f86afcdf27d293b2385f34b2db0bfc52b92ba
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-22 15:43:28 +0200

    First 2 pop test's passing (emtpy & 1 element)

--- Swift/Stack/Stack/Stack.swift
+++ Swift/Stack/Stack/Stack.swift
@@ -10,12 +10,22 @@ import Foundation
 
 class Stack<T>
 {
+    private var stack = [T]()
+    
+    
+    init() { }
+    
+    init(initialElements: [T]) {
+        self.stack.appendContentsOf(initialElements)
+    }
+    
+    
     func push(element: T) {
         
     }
     
     func pop() -> T? {
         
-        return nil
+        return self.stack.last
     }
 }
--- Swift/Stack/StackTests/StackTests.swift
+++ Swift/Stack/StackTests/StackTests.swift
@@ -11,26 +11,14 @@ import XCTest
 
 class StackTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testEmptyStackPopReturnsNil() {
+        XCTAssertNil(Stack().pop())
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testPopRetunsAnItem() {
+        
+        let stack = Stack(initialElements: [1])
+        
+        XCTAssertEqual(1, stack.pop())
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
