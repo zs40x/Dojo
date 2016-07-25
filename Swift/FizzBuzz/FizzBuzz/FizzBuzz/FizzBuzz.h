5e4c96743945251068b92d4e25fd62e08b561011
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-25 16:30:10 +0200

    Added FizzBuzz Kata project (empty)

---
+++ Swift/FizzBuzz/FizzBuzz/FizzBuzz/FizzBuzz.h
@@ -0,0 +1,19 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Stefan Mehnert on 25/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
